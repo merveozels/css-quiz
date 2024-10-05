a55f3f7605ca8945b870ac9839a821d8f48d2e9e
Merve Ozel <leylakmerve@gmail.com>
2024-10-05 15:01:49 +0300

    quiz

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+
+function result (){
+  let result = 0;
+
+  if (document.getElementById('true1').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true2').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true3').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true4').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true5').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true6').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true7').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true8').checked){
+    result = result + 1;
+  }
+
+  if (document.getElementById('true9').checked){
+    result = result + 1;
+  }
+  
+  if (document.getElementById('true10').checked){
+    result = result + 1;
+  }
+
+  alert('Toplam' + ' ' + result +  ' '  + 'soruyu doğru bildin');
+}
