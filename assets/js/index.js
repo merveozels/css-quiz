588f6e2225da2ddadf5d7f02ab12a0f51169145f
Merve Ozel <leylakmerve@gmail.com>
2024-10-06 23:45:20 +0300

    background color eklendi

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,56 @@
 
-function result (){
-  let result = 0;
+// function result (){
+//   let result = 0;
 
-  if (document.getElementById('true1').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true1').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true2').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true2').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true3').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true3').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true4').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true4').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true5').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true5').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true6').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true6').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true7').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true7').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true8').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true8').checked){
+//     result = result + 1;
+//   }
 
-  if (document.getElementById('true9').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true9').checked){
+//     result = result + 1;
+//   }
   
-  if (document.getElementById('true10').checked){
-    result = result + 1;
-  }
+//   if (document.getElementById('true10').checked){
+//     result = result + 1;
+//   }
 
-  alert('Toplam' + ' ' + result +  ' '  + 'soruyu doğru bildin');
+//   alert('Toplam' + ' ' + result +  ' '  + 'soruyu doğru bildin');
+// }
+
+// 
+
+
+function result (){
+  document.querySelector('ol').classList.add('solved');
+  let total = document.querySelectorAll('.true:checked').length;
+  alert('Toplam' + ' ' + total +  ' '  + 'soruyu doğru bildin');
 }
+
